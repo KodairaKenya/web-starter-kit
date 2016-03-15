cb4b0e849c32439a80bf510742a8061bbbb456cb
Shuma Mizuno <s.mizuno@s-arcana.co.jp>
2016-03-16 01:01:06 +0900

    【Add】gulpfile.js / v4.0

---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+'use strict';
+
+// Plugin load
+var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var del         = require('del');
+var pagespeed   = require('psi');
+var reload      = browserSync.reload;
+
+// Source path
+var path = {
+  html     : 'app/jade/**.jade',
+  css      : 'app/**/*.scss',
+  cssLib   : 'app/lib/*.css',
+  js       : 'app/*.js',
+  jsLib    : 'app/lib/*.js',
+  img      : 'app/img/*/*.*',
+  htmlDest : 'build/',
+  cssDest  : 'build/css',
+  jsDest   : 'build/js',
+  imgDest  : 'build/img',
+};
+
+// Jade compile
+function html() {
+  return gulp.src(path.html)
+    .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+    .pipe($.sourcemaps.init())
+    .pipe($.jade({
+      pretty: true
+    }))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(path.htmlDest))
+    .pipe($.size({title: 'jade'}));
+}
+
+// Sass compile
+function css() {
+  return gulp.src(path.css)
+  .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+  .pipe($.sourcemaps.init())
+  .pipe($.sass())
+  .pipe($.pleeease({
+    autoprefixer: ['last 2 versions'],
+    minifier: false
+  }))
+  .pipe($.size({title: 'sass'}))
+  .pipe($.concat('common.css'))
+  .pipe($.sourcemaps.write('./'))
+  .pipe(gulp.dest(path.cssDest));
+}
+
+// CSS Plugin concat
+function cssLib() {
+  return gulp.src(path.cssLib)
+  .pipe($.plumber())
+  .pipe($.concat('lib.css'))
+  .pipe(gulp.dest(path.cssDest));
+}
+
+// JavaScript optimaize
+function js() {
+  return gulp.src(path.js)
+  .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+  .pipe($.sourcemaps.write())
+  .pipe($.concat('common.js'))
+  .pipe($.uglify())
+  .pipe($.size({title: 'js'}))
+  .pipe($.sourcemaps.write('.'))
+  .pipe(gulp.dest(path.jsDest));
+}
+
+// JavaScript plugin concat
+function jsLib() {
+  return gulp.src(path.js)
+  .pipe($.concat('lib.js'))
+  .pipe($.uglify())
+  .pipe(gulp.dest(path.jsDest));
+}
+
+// Image optimize
+function img() {
+  return gulp.src(path.img, { since: gulp.lastRun(img) })
+  .pipe($.imagemin({
+    progressive: true,
+    interlaced: true
+  }))
+  .pipe(gulp.dest(path.imgDest))
+  .pipe($.size({title: 'img'}));
+}
+
+// //  Copy files
+// function copy() {
+//   gulp.src([
+//     'app/*',
+//   ], {
+//     dot: true
+//   }).pipe(gulp.dest('dist'))
+//     .pipe($.size({title: 'copy'}))
+// }
+
+
+// Build folder delete
+function clean(cb) {
+  return del('build', cb);
+}
+
+// Google page speed
+function pageSpeed(cb)  {
+  pagespeed('example.com', {
+    strategy: 'mobile'
+  }, cb);
+}
+
+// Local server
+function bs(cb) {
+  return browserSync.init(null, {
+    server: {
+      baseDir: 'build'
+    },
+    ghostMode: false,
+    notify: false
+  }, cb);
+}
+
+// Watch
+gulp.task('watch', function(cb) {
+  gulp.watch(path.html, gulp.series(html, reload));
+  gulp.watch(path.css, gulp.series(css, reload));
+  gulp.watch(path.cssLib, gulp.series(cssLib, reload));
+  gulp.watch(path.js, gulp.series(js, reload));
+  gulp.watch(path.jsLib, gulp.series(jsLib, reload));
+  gulp.watch(path.img, gulp.series(img, reload));
+  cb();
+});
+
+// Default Build
+gulp.task('build', gulp.series (
+  clean,
+  gulp.parallel(html, css, cssLib, js, jsLib, img /*copy*/),
+  bs
+));
+gulp.task(pageSpeed);
+
+// Default Build
+gulp.task('default', gulp.series('build', 'watch'));
